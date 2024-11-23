eebc565b769243304d237ddc2d17a5c6d64e5c0e
aheshmat <aheshmat@kn-it.com>
2024-11-23 14:49:46 +0200

    update readme file

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# task-management-system
+
+# Task Management System
+
+## Introduction
+The **Task Management System** is a RESTful application built with **Spring Boot**. It provides features like task management, user authentication, and role-based access control. This guide will help you set up and run the application locally, including database configuration and initializing sample data using the `data.sql` file.
+
+---
+
+## Usage
+
+### 1. **Clone the Repository**
+Clone the repository to your local machine:
+```bash
+git clone https://github.com/NasserHeshmat/task-management-system.git
+cd task-management-system
+```
+
+---
+
+### 2. **Configure the Database**
+The application uses an Oracle database by default. Follow these steps to set up the database connection:
+
+1. **Update `application.properties`**
+   Open the `src/main/resources/application.properties` file and configure the database connection details:
+   ```properties
+   spring.datasource.url=jdbc:oracle:thin:@localhost:1521:XE
+   spring.datasource.username=your_db_username
+   spring.datasource.password=your_db_password
+   spring.jpa.hibernate.ddl-auto=update
+   ```
+
+3. **Initialize the Database with `data.sql`**
+   - The file `src/main/resources/data.sql` contains seed data for the application.
+   - make sure at your first run from the following property is set:
+     spring.sql.init.mode=always
+   - after that you can make it a:
+     spring.sql.init.mode=never
+
+
+---
+
+### 3. **Build and Run the Application**
+1. **Build the project**:
+   ```bash
+   mvn clean install
+   ```
+
+2. **Run the application**:
+   ```bash
+   mvn spring-boot:run
+   ```
+
+3. **Test the Application**:
+   - Access the application via `http://localhost:8097`.
+   - And you can import the postman collection to test the apis 
--- src/main/java/com/demo/task/management/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/demo/task/management/service/impl/AuthenticationServiceImpl.java
@@ -1,6 +1,5 @@
 package com.demo.task.management.service.impl;
 
-
 import com.demo.task.management.exception.CustomException;
 import com.demo.task.management.model.AuthenticationRequest;
 import com.demo.task.management.model.AuthenticationResponse;
@@ -19,11 +18,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
-
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import static com.demo.task.management.constant.ErrorMessages.*;
 
@@ -33,7 +29,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
     private final JwtUtil jwtUtil;
-    private final Set<String > usedRefreshTokens = new HashSet<>();
     private final UserRepository userRepository;
     @Override
     public AuthenticationResponse createAuthenticationTokens(AuthenticationRequest authRequest) {
@@ -59,16 +54,11 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         if (jwtUtil.isTokenExpired(refreshTokenRequest.getRefreshToken()) || !jwtUtil.isRefreshToken(refreshTokenRequest.getRefreshToken())) {
             throw new CustomException(INVALID_TOKEN, HttpStatus.FORBIDDEN);
         }
-        if (usedRefreshTokens.contains(refreshTokenRequest.getRefreshToken())) {
-            throw new CustomException(USED_REFRESH_TOKEN, HttpStatus.FORBIDDEN);
-        }
-        usedRefreshTokens.add(refreshTokenRequest.getRefreshToken());
         List<String> roles = jwtUtil.extractRoles(refreshTokenRequest.getRefreshToken());
 
         Collection<? extends GrantedAuthority> authorities = roles.stream()
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
-
 
         String username = jwtUtil.extractUsername(refreshTokenRequest.getRefreshToken());
         return new RefreshTokenResponse(jwtUtil.generateToken(username, false,authorities));
