bf9b1744af93d4efc728f686bae50fccdc40859d
aheshmat <aheshmat@kn-it.com>
2024-11-23 15:36:59 +0200

    moving static roles

---
+++ src/main/java/com/demo/task/management/constant/Roles.java
@@ -0,0 +1,8 @@
+package com.demo.task.management.constant;
+
+public final class Roles {
+
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+}
--- src/main/java/com/demo/task/management/controller/TaskController.java
+++ src/main/java/com/demo/task/management/controller/TaskController.java
@@ -8,13 +8,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import java.util.List;
 
 import static com.demo.task.management.constant.ErrorMessages.TASK_DELETED;
+import static com.demo.task.management.constant.Roles.ROLE_ADMIN;
+import static com.demo.task.management.constant.Roles.ROLE_USER;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
@@ -24,12 +25,12 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping
-    @RolesAllowed({"ROLE_ADMIN", "ROLE_USER"})
+    @RolesAllowed({ROLE_USER, ROLE_ADMIN})
     public Page<Task> getAllTasks(Pageable pageable) {
         return taskService.getAllTasks(pageable);
     }
 
-    @RolesAllowed({"ROLE_ADMIN", "ROLE_USER"})
+    @RolesAllowed({ROLE_ADMIN, ROLE_USER})
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
         return taskService.getTaskById(id)
@@ -37,23 +38,23 @@ public class TaskController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @RolesAllowed("ROLE_ADMIN")
+    @RolesAllowed(ROLE_ADMIN)
     @PostMapping
     public Task createTask(@RequestBody @Valid TaskCreateDto task) {
         return taskService.createTask(task);
     }
-    @RolesAllowed("ROLE_ADMIN")
+    @RolesAllowed(ROLE_ADMIN)
     @PutMapping("/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody @Valid TaskCreateDto task) {
         return ResponseEntity.ok(taskService.updateTask(id, task));
     }
-    @RolesAllowed("ROLE_ADMIN")
+    @RolesAllowed(ROLE_ADMIN)
     @DeleteMapping("/{id}")
     public ResponseEntity<ConfirmationResponse> deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
         return ResponseEntity.ok(new ConfirmationResponse(TASK_DELETED));
     }
-    @RolesAllowed({"ROLE_ADMIN", "ROLE_USER"})
+    @RolesAllowed({ROLE_ADMIN, ROLE_USER})
     @PostMapping("/search")
     public List<Task> searchTasks(@RequestBody Task task) {
         return taskService.searchTasks(task);
